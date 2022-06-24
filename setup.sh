c4ccc94d27a0cef777d68c28d8fb88630fbad089
Enkel Prifti <43306174+enkelprifti98@users.noreply.github.com>
2022-06-24 18:21:09 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 
 # Install XFCE GUI, VNC server, and other necessary packages
 
-apk add --no-cache ca-certificates bash curl jq openssl sudo xvfb x11vnc xfce4 xfce4-terminal faenza-icon-theme bash procps nano git pciutils gzip p7zip cpio tar unzip xarchiver
+apt update && apt install -y ca-certificates curl openssl sudo xvfb x11vnc xfce4 xfce4-terminal faenza-icon-theme bash procps nano git
 
-# Set VNC password: ("alpine" but you can set it to whatever)
+# Set VNC password: ("admin" but you can set it to whatever)
 
-mkdir -p /root/.vnc && x11vnc -storepasswd alpine /root/.vnc/passwd
+mkdir -p /root/.vnc && x11vnc -storepasswd admin /root/.vnc/passwd
 
 # Start GUI and VNC server services
 
@@ -19,35 +19,20 @@ nohup startxfce4 > /dev/null 2>&1 &
 
 nohup x11vnc -xkb -noxrecord -noxfixes -noxdamage -display :99 -forever -bg -rfbauth /root/.vnc/passwd -users root -rfbport 5900 > /dev/null 2>&1 &
 
-# This network config file with a loopback interface is needed for libvirtd and noVNC (loopback) services to start successfully
-
-cat > /etc/network/interfaces <<EOF
-auto lo
-iface lo inet loopback
-EOF
-
 # Install KVM hypervisor
 
-apk add libvirt-daemon qemu-img qemu-system-x86_64 qemu-modules virt-manager
-rc-update add libvirtd
+apt install -y qemu qemu-kvm libvirt-daemon libvirt-clients bridge-utils virt-manager
 modprobe tun
 modprobe br_netfilter
 grep -q -E 'vmx' /proc/cpuinfo && modprobe kvm-intel
 grep -q -E 'svm' /proc/cpuinfo && modprobe kvm-amd
+service libvirtd start
 
-# Enable VFIO PCI Passthrough
-
-modprobe vfio_pci
-modprobe vfio_iommu_type1
-chown qemu /dev/vfio/vfio
-
-# Start libvirtd service
-
-rc-service libvirtd start
 
 # Install web-browser (Firefox works, Chromium seems to throw an I/O error and doesn't launch)
 
-apk add firefox-esr
+apt install -y firefox
+xdg-settings set default-web-browser firefox.desktop
 
 # Install NoVNC (VNC client over http)
 
@@ -77,11 +62,11 @@ PUBLIC_IP=$(curl -s https://metadata.platformequinix.com/metadata | jq -r ".netw
 nohup filebrowser -r /root -a $PUBLIC_IP -p 8080 > /dev/null 2>&1 &
 
 printf "\n\n"
-echo "The desktop environment is available at:"
+echo "The ISO installation environment is available at:"
 printf "\n"
 echo "http://$PUBLIC_IP/"
 printf "\n"
-echo "The file transfer portal is available at:"
+echo "The File Transfer portal is available at:"
 printf "\n"
 echo "http://$PUBLIC_IP:8080/"
 printf "\n\n"
