ba882dcc590facee3e83016f1048ad69a3988b6d
Enkel Prifti <43306174+enkelprifti98@users.noreply.github.com>
2024-09-09 20:57:18 -0400

    Update setup-v2.sh

--- setup-v2.sh
+++ setup-v2.sh
@@ -970,6 +970,18 @@ systemctl unmask networking
 systemctl enable networking
 systemctl restart networking
 
+systemctl stop systemd-networkd.socket systemd-networkd \
+networkd-dispatcher systemd-networkd-wait-online
+
+systemctl disable systemd-networkd.socket systemd-networkd \
+networkd-dispatcher systemd-networkd-wait-online
+
+systemctl mask systemd-networkd.socket systemd-networkd \
+networkd-dispatcher systemd-networkd-wait-online
+apt-get --assume-yes purge nplan netplan.io
+
+ifup $MANAGEMENT_IF_NAME
+ifdown $MANAGEMENT_IF_NAME
 ifup $MANAGEMENT_IF_NAME
 
 ip route del default
